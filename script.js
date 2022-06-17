7ae77f720350ce15b6639da95f7f5f31bd0b7d39
ZofiaGa <104167162+ZofiaGa@users.noreply.github.com>
2022-06-17 16:59:47 +0200

    Add files via upload
    1st commit
    rating component - responsive, mobile first, flexbox, with SASS

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const submitPage = document.querySelector(".submit-page");
+const numberToSelect = document.querySelectorAll(".number");
+const submitBtn = document.querySelector(".submit-btn");
+const thankYouPage = document.querySelector(".thank-you-page");
+const selectedNum = document.querySelector(".selected-num");
+
+for (let i = 0; i < numberToSelect.length; i++) {
+  numberToSelect[i].addEventListener("onmouseover",  (e) => {
+    e.target.style.backgroundColor = "#fc7614"; // orange
+    e.target.style.color = "#ffffff";
+  });
+
+  numberToSelect[i].addEventListener("click",  () => {
+    selectedNum.textContent =
+      "You selected " + numberToSelect[i].textContent + " out of 5";
+
+    for (let k = 0; k < numberToSelect.length; k++) {
+      if (numberToSelect[k].style.backgroundColor !== "#262e38") {
+        numberToSelect[k].style.backgroundColor = "#262e38"; // dark blue
+        numberToSelect[k].style.color = "#969fad";
+      }
+    }
+    numberToSelect[i].style.backgroundColor = "#969fad"; // medium grey
+    numberToSelect[i].style.color = "#ffffff";
+
+  });
+}
+
+submitBtn.addEventListener("click",  () => {
+  if (selectedNum.textContent.trim() === "Nothing selected") {
+    alert("You didn't select a rating. \nPlease select one. \nThank you!");
+  } else {
+    submitPage.classList.add("hidden");
+    thankYouPage.classList.remove("hidden");
+  }
+});
